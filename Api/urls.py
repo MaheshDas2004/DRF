870fcec03153a3bcec52165f864bf793c7892c2c
Mahesh Das <Mahesh.das2205@gmail.com>
2025-08-01 14:54:31 +0530

    learned how to create api easily using  mixins,generics, viewsets and modelviewsets

--- Api/urls.py
+++ Api/urls.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+# for using viewset
+router=DefaultRouter()
+router.register('employees',views.EmployeeViewSet,basename='employee')
+
 urlpatterns = [
     # Api Endpoints
     path('students/',views.studentsView),
     path('students/<int:id>/',views.single_studentsView),
 
-    path('Employees/',views.Employees.as_view()),#class based views
-    path('Employees/<int:id>/',views.Single_Employee.as_view())#class based views
+    # path('Employees/',views.Employees.as_view()),#class based views
+    # path('Employees/<int:pk>/',views.Single_Employee.as_view())#class based views
+    path('',include(router.urls))
 ]
--- Api/views.py
+++ Api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import JsonResponse
 from students.models import Students
 from . serializers import StudentSerializers, EmployeeSerializers
@@ -8,6 +8,7 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from Employees.models import Employee
 from django.http import Http404
+from rest_framework import mixins,generics,viewsets
 # Create your views here.
 # Function Based views
 @api_view(['GET','POST'])
@@ -55,46 +56,122 @@ def single_studentsView(request,id):
         student.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-class Employees(APIView):
-    def get(self,request):
-        employees=Employee.objects.all()
-        serializer=EmployeeSerializers(employees,many=True)
+# API Creation manually without mixins
+# class Employees(APIView):
+#     def get(self,request):
+#         employees=Employee.objects.all()
+#         serializer=EmployeeSerializers(employees,many=True)
+#         return Response(serializer.data,status=status.HTTP_200_OK)
+    
+#     def post(self,request):
+#         serializer=EmployeeSerializers(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data,status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+# class Single_Employee(APIView):
+#     def get_employee(self,id):
+#         try:
+#             return Employee.objects.get(id=id)
+#         except Employee.DoesNotExist:
+#             raise Http404
+#     def get(self,request,id):
+#         employee=self.get_employee(id)
+#         serializer=EmployeeSerializers(employee)
+#         return Response(serializer.data,status=status.HTTP_200_OK)
+#     def put(self,requset,id):
+#         employee=self.get_employee(id)
+#         serializer=EmployeeSerializers(employee,data=requset.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data,status=status.HTTP_200_OK)
+#         else:
+#             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+#     def delete(self,requset,id):
+#         employee=self.get_employee(id)
+#         employee.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+# Using mixins:"Mixins DRF me chhoti-chhoti ready-made classes hoti hain jo GET, POST, PUT, DELETE jaise kaam asaani se karne deti hain bina pura code likhe."Ye tumhare views me CRUD functionality add karte hain reusability aur simplicity ke liye."
+# class Employees(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+#     #variables ka name by default hota ha cant change the name
+#     queryset=Employee.objects.all()
+#     serializer_class= EmployeeSerializers
+
+#     def get(self,request):
+#         return self.list(request)
+    
+#     def post(self,requset):
+#         return self.create(requset)
+    
+# class Single_Employee(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin,generics.GenericAPIView):
+#     queryset=Employee.objects.all()
+#     serializer_class= EmployeeSerializers
+
+#     def get(self,request,pk):# yahan pr pk hi by default use hoga manual apis me ham koi bhi naam daal skte ha but mixins me by default pk use krna padta ha
+#         return self.retrieve(request,pk)
+    
+#     def put(self,requset,pk):
+#         return self.update(requset,pk)
+    
+#     def delete(self,request,pk):
+#         return self.destroy(request,pk)
+
+
+# using Genrics :"Django REST Framework ke generics ready-made views hote hain jo common CRUD operations ke liye code aur short kar dete hain."Inme sab kuch built-in hota hai — tumhe sirf queryset aur serializer_class dena hota hai.
+
+# can use "generics.ListCreateAPIView" for implementing create and update all at once
+"""
+class Employees(generics.CreateAPIView,generics.ListAPIView):
+    queryset=Employee.objects.all()
+    serializer_class=EmployeeSerializers
+
+# can use combination of 3 api views instead of using 3,  "generics.RetrieveUpdateDestroyAPIView"
+class Single_Employee(generics.RetrieveAPIView,generics.UpdateAPIView,generics.DestroyAPIView):
+    queryset=Employee.objects.all()
+    serializer_class=EmployeeSerializers
+    lookup_field='pk'
+"""
+
+
+# Using viewsets:"ViewSet ek DRF class hai jo ek hi jagah se multiple actions (GET, POST, PUT, DELETE) handle kar leta hai.""Ye routing ke liye routers ka use karta hai, jisse tumhara URL config bhi automatic ho jaata hai."
+"""
+class EmployeeViewSet(viewsets.ViewSet):
+    def list(self,request):
+        queryset=Employee.objects.all()
+        serializer=EmployeeSerializers(queryset,many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
     
-    def post(self,request):
+    def create(self,request):
         serializer=EmployeeSerializers(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
+            return Response(serializer.data,status=status.HTTP_200_OK)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-
-class Single_Employee(APIView):
-    def get_employee(self,id):
-        try:
-            return Employee.objects.get(id=id)
-        except Employee.DoesNotExist:
-            raise Http404
-    def get(self,request,id):
-        employee=self.get_employee(id)
+    
+    def retrieve(self,request,pk=None):
+        employee=get_object_or_404(Employee,pk=pk)
         serializer=EmployeeSerializers(employee)
         return Response(serializer.data,status=status.HTTP_200_OK)
-    def put(self,requset,id):
-        employee=self.get_employee(id)
-        serializer=EmployeeSerializers(employee,data=requset.data)
+    
+    def update(self,request,pk=None):
+        employee=get_object_or_404(Employee,pk=pk)
+        serializer=EmployeeSerializers(employee,request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=status.HTTP_200_OK)
-        else:
-            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-    def delete(self,requset,id):
-        employee=self.get_employee(id)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,request,pk=None):
+        employee=get_object_or_404(Employee,pk=pk)
         employee.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+"""
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset=Employee.objects.all()
+    serializer_class=EmployeeSerializers
 
-
-
-
-            
 
 
 
